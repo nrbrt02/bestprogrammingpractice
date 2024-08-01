056718dbd4a66720918205a5b0a487cc8099fb23
nrbrt2002 <nrbrt2002@gmail.com>
2024-08-01 17:05:47 +0530

    logs done

--- src/main/java/com/example/Submitted1.java
+++ src/main/java/com/example/Submitted1.java
@@ -5,22 +5,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Submitted1 extends HttpServlet {
-//    private static final Logger logger = LogManager.getLogger(Submitted1.class);
+    private static final Logger logger = LogManager.getLogger(Submitted1.class);
 
     public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
         String id = req.getParameter("id");
         String names = req.getParameter("names");
         
-//        logger.debug("Received request with ID: " + id + " and Names: " + names);
+        logger.debug("Received request with ID: " + id + " and Names: " + names);
         
         PrintWriter out = res.getWriter();
-        out.println(id);
-        out.println(names);
+        out.println("Submmited Id is:"+id);
+        out.println("Names are :"+names);
         
-//        logger.info("Response sent for ID: " + id + " and Names: " + names);
+        logger.info("Response sent for ID: " + id + " and Names: " + names);
     }
 }
